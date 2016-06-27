1543c67b9b110c7235a4ce31024fbd36060a4045
邢俊杰 <67541967@qq.com>
2016-06-27 15:37:42 +0800

    以管理员权限启动

--- Saber/main.cpp
+++ Saber/main.cpp
@@ -1,9 +1,31 @@
 #include "MainWidget.h"
 #include <QApplication>
 #include <QtFlexStyle.h>
+#include <QProcess>
+#include <QDebug>
+
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+    if (getuid() != 0)
+    {
+        QString aScript("do shell script \"\\\"");
+        aScript += argv[0];
+        aScript += "\\\"\" with administrator privileges";
+
+        QString osascript = "/usr/bin/osascript";
+        QStringList processArguments;
+        processArguments << "-l" << "AppleScript";
+
+        QProcess p;
+        p.start(osascript, processArguments);
+        p.write(aScript.toUtf8());
+        p.closeWriteChannel();
+        p.waitForReadyRead(-1);
+        return 0;
+    }
+
     QApplication a(argc, argv);
     QApplication::setStyle(new FlexStyle());
 
