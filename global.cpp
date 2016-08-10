62f127816e5d237614d3664f1bf7edfe4df3b982
网事如风 <67541967@qq.com>
2016-08-10 17:09:26 +0800

    补上忘了添加的两个文件

---
+++ global.cpp
@@ -0,0 +1,7 @@
+//
+// Created by System Administrator on 16/8/10.
+//
+
+#include "global.h"
+
+uint64_t g_highlightAddress = 0;
---
+++ global.h
@@ -0,0 +1,9 @@
+//
+// Created by System Administrator on 16/8/10.
+//
+
+#pragma once
+
+#include <cstdint>
+
+extern uint64_t g_highlightAddress;
